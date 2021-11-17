3cd768433d12302a532f6260291ec020d0cc307f
rahman973 <chrahman973@gmail.com>
2021-11-18 02:24:29 +0500

    Health checker added

--- v1/routes/index.js
+++ v1/routes/index.js
@@ -3,4 +3,5 @@ var router = express.Router();
 
 router.use("/auth", require("./Auth"));
 router.use("/expenses", require("./Expenses"));
+router.use("/health", require("./Health"));
 module.exports = router;
